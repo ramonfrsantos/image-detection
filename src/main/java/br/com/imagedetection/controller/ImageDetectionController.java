fb984b850a6889faa9ea773cbe339ede8efb080a
Ramon Felipe Ribeiro Santos <ramonfrsantos@outlook.com>
2022-10-06 17:44:37 -0300

    comentando código

--- src/main/java/br/com/imagedetection/controller/ImageDetectionController.java
+++ src/main/java/br/com/imagedetection/controller/ImageDetectionController.java
@@ -29,12 +29,16 @@ public class ImageDetectionController {
 	@PostMapping("/image-analysis")
 	public ImageResponseDTO getAnalysis(@RequestBody ImageModelDTO model) {
 	
+		// carrega a biblioteca opencv
 		nu.pattern.OpenCV.loadShared();
 		
+		// declara se semelhanca detectada como falso
 		boolean imageDetected = false;
 		
+		//caminho arquivo de entrada
 		String inputFilePath = "./test_temp." + model.getFormato();
 		
+		// --- converte o base64 enviado no body da requisição para arquivo ---
 		byte[] data = org.apache.tomcat.util.codec.binary.Base64.decodeBase64(model.getBase64());
 		
 		try (OutputStream imageInFile = new FileOutputStream(inputFilePath)) {
@@ -46,27 +50,41 @@ public class ImageDetectionController {
 		}
 		
         File inputFile = new File(inputFilePath);
+        
+        //-----
 		
+        // lê o arquivo de entrada e converte para o formato Mat
 		Mat src = Imgcodecs.imread(inputFilePath);
 		
+		// caminho do arquivo resultado do treinamento, com especificações do modelo treinado
 		String xmlFile = "./cascade.xml";
+		
 		CascadeClassifier cc = new CascadeClassifier(xmlFile);
 		
 		MatOfRect imgDetection = new MatOfRect();
+		
+		// detecta se a imagem de entrada da match com o modelo
 		cc.detectMultiScale(src, imgDetection);
+		
+		// se acha alguma semelhança, seta o boolean para true
 		if(imgDetection.toArray().length > 0) {
 			imageDetected = true;
 		}
+		
 		System.out.println("Image detected: " + imageDetected);
 		
+		// cria retangulos para indicar as semelhanças na imagem
 		for(Rect rect: imgDetection.toArray()) {
 			Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y+rect.height), new Scalar(0,0,255), 3);
 		}
 		
+		// --- escreve na imagem de saída a imagem pós-análise
 		String outputFilePath = "./test_out." + model.getFormato();
 		
 		Imgcodecs.imwrite(outputFilePath, src);
+		//----
 		
+		// --- converte o arquivo de saída em base64 para retorno
         String base64OutputFile = "";
 		
 		File outputFile = new File(outputFilePath);
@@ -81,12 +99,15 @@ public class ImageDetectionController {
         } catch (IOException ioe) {
             System.out.println("Exception while reading the file " + ioe);
         }
+        //----
         
+        // exclui os arquivos temporarios criados        
         inputFile.delete();
         outputFile.delete();
 	    
-		System.out.println("Program finished!");
-	    		
+		System.out.println("Program finished!\n");
+		
+		// retorna a imagem criada pós-análise	    		
 		return new ImageResponseDTO(base64OutputFile, model.getFormato());		
 	}
 	
